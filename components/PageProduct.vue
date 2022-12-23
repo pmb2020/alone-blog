0ab04aff89d8af023d989413cc1b8c0c758a1e09
pmb2020 <pmb2020@163.com>
2022-12-23 15:52:44 +0800

    首页产品图替换

--- components/PageProduct.vue
+++ components/PageProduct.vue
@@ -11,34 +11,40 @@
 			</div>
 		</div>
 		<div class="grid grid-cols-3 gap-10">
-			<div>
+			<div class="img-txt-box">
 				<nuxt-link to="/product/3">
-					<img src="http://cdn.xfrec.com/vaa/A9.jpg" alt="">
+					<img src="http://39.105.2.43:90/images/2022/12-23/NxmGcTdbWeIXaJ9LHy1r6hu59NEh4dfbtocYrQvw.jpg" alt="">
+					<h3>先Phone X9</h3>
 				</nuxt-link>
 			</div>
-			<div>
+			<div class="img-txt-box">
 				<nuxt-link to="/product/4">
-					<img src="http://cdn.xfrec.com/vaa/A11.jpg" alt="">
+					<img src="http://39.105.2.43:90/images/2022/12-23/N8nnqvgrdekFj3I70RBlRM7i9Nw0pagbzFGj0LTu.jpg" alt="">
+					<h3>先Phone 11</h3>
 				</nuxt-link>
 			</div>
-			<div>
+			<div class="img-txt-box">
 				<nuxt-link to="/product/5">
-					<img src="http://cdn.xfrec.com/vaa/A12.jpg" alt="">
+					<img src="http://39.105.2.43:90/images/2022/12-23/0lhv8O3UyH8C74XR9oBRXUN13oqnO4AHioz34Hag.jpg" alt="">
+					<h3>先Phone 12</h3>
 				</nuxt-link>
 			</div>
-			<div>
+			<div class="img-txt-box">
 				<nuxt-link to="/product/6">
-					<img src="http://cdn.xfrec.com/vaa/A13.jpg" alt="">
+					<img src="http://39.105.2.43:90/images/2022/12-23/qFEUrGnE5pog5ILTSmnSRSyo7C9IHgpbfjPXnkLw.jpg" alt="">
+					<h3>先Phone 13</h3>
 				</nuxt-link>
 			</div>
-			<div>
+			<div class="img-txt-box">
 				<nuxt-link to="/product/7">
-					<img src="http://cdn.xfrec.com/vaa/A15.jpg" alt="">
+					<img src="http://39.105.2.43:90/images/2022/12-23/3Vibgd0bKUe50JPEy6e6UIeNNq1zNbvDTIuVYuKp.jpg" alt="">
+					<h3>先Phone 15</h3>
 				</nuxt-link>
 			</div>
-			<div>
+			<div class="img-txt-box">
 				<nuxt-link to="/product/8">
-					<img src="http://cdn.xfrec.com/vaa/A16.jpg" alt="">
+					<img src="http://39.105.2.43:90/images/2022/12-23/TBnbg9iLnG3vfgd5cu9BuFKtEBHD0CNj9SR4Ea67.jpg" alt="">
+					<h3>先Phone 16</h3>
 				</nuxt-link>
 			</div>
 		</div>
@@ -48,5 +54,15 @@
 <script>
 </script>
 
-<style>
+<style lang="scss">
+	.img-txt-box{
+		color: #333;
+		img{
+			border-radius: 10px;
+		}
+		h3{
+			margin-top: 5px;
+			text-align: center;
+		}
+	}
 </style>
